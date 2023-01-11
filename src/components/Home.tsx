d34373df5a2b205a020695399340d0ed0f777063
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-11 13:31:15 +0100

    feat: add battery row

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Battery } from './Battery'
+import { BatteryType } from './Home.types'
 import { Scene } from './Scene'
 
 export const Home = () => {
+    const [batteries, setBatteries] = useState<BatteryType[]>([])
+
+    const onClear = () => {
+        setBatteries([])
+    }
+
+    const onBatteryRowAdd = () => {
+        setBatteries((previousState) => {
+            const lastBattery = previousState.at(-1) ?? {
+                x: 0,
+                y: 0,
+                z: 0
+            }
+
+            return [
+                ...previousState,
+                ...[...new Array(5)].map((_, index) => {
+                    return {
+                        x: lastBattery.y + (index * 2),
+                        z: lastBattery.z + 2,
+                        y: lastBattery.y
+                    }
+                })
+            ]
+        })
+    }
+
     return (
-        <Scene>
-            <Battery position={[-4, 0, 0]} />
-            <Battery position={[0, 0, 0]} />
-            <Battery position={[5, 0, 0]} />
-        </Scene>
+        <>
+            <button onClick={onBatteryRowAdd}>Add Battery Row</button>
+            <button onClick={onClear}>Clear</button>
+            <Scene>
+                {batteries.map((battery) => {
+                    return (
+                        <Battery
+                            position={[
+                                battery.x,
+                                battery.y,
+                                battery.z
+                            ]}
+                        />
+                    )
+                })}
+            </Scene>
+        </>
     )
 }
---
+++ src/components/Home.types.ts
@@ -0,0 +1,5 @@
+export type BatteryType = {
+    x: number
+    y: number
+    z: number
+}
