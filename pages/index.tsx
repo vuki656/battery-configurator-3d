e5fa2e441144529fb5e1073c1980114ea401a13e
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-11 11:59:20 +0100

    feat: basic example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
-export default function Home() {
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef, useState } from 'react'
+
+function Box(props) {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef()
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    // Return the view, these are regular Threejs elements expressed in JSX
     return (
-        <p>
-            hi
-        </p>
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
     )
 }
+
+const Home = () => {
+    return (
+        <Canvas style={{
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+            <OrbitControls />
+        </Canvas>
+    )
+}
+
+export default Home
