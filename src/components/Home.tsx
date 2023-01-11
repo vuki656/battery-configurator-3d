b83c8608bbceda31ea5f06ac30b162dcb6a35b94
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-11 13:42:33 +0100

    feat: add stack

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,10 @@ import { Battery } from './Battery'
 import { BatteryType } from './Home.types'
 import { Scene } from './Scene'
 
+const BATTERY_X_DISTANCE = 2
+const BATTERY_Z_DISTANCE = 2
+const BATTERY_Y_DISTANCE = 4
+
 export const Home = () => {
     const [batteries, setBatteries] = useState<BatteryType[]>([])
 
@@ -23,9 +27,23 @@ export const Home = () => {
                 ...previousState,
                 ...[...new Array(5)].map((_, index) => {
                     return {
-                        x: lastBattery.y + (index * 2),
-                        z: lastBattery.z + 2,
+                        x: index * BATTERY_X_DISTANCE,
+                        z: lastBattery.z + BATTERY_Z_DISTANCE,
                         y: lastBattery.y
+                    }
+                })
+            ]
+        })
+    }
+
+    const onBatteryStackAdd = () => {
+        setBatteries((previousState) => {
+            return [
+                ...previousState,
+                ...previousState.map((battery) => {
+                    return {
+                        ...battery,
+                        y: battery.y + BATTERY_Y_DISTANCE
                     }
                 })
             ]
@@ -35,6 +53,7 @@ export const Home = () => {
     return (
         <>
             <button onClick={onBatteryRowAdd}>Add Battery Row</button>
+            <button onClick={onBatteryStackAdd}>Add Battery Stack</button>
             <button onClick={onClear}>Clear</button>
             <Scene>
                 {batteries.map((battery) => {
