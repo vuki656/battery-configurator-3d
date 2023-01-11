5c30e6c1d40f5f84b6692ce5c05447f9a61a7bdf
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-11 11:50:41 +0100

    feat: lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        require.resolve("@rimac-technology/style-guide/eslint/core"),
+        require.resolve("@rimac-technology/style-guide/eslint/next"),
+        require.resolve("@rimac-technology/style-guide/eslint/react"),
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    ignorePatterns: [
+        "next.config.js",
+        ".eslintrc.js",
+    ],
+}
